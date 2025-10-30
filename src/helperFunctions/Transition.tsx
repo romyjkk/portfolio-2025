3423b59ea483b6d547fdfcdaf568986e9278c326
romyjkk <romyjongkees@gmail.com>
2025-10-30 17:13:24 +0100

    Mobile page transition

--- src/helperFunctions/Transition.tsx
+++ src/helperFunctions/Transition.tsx
@@ -11,74 +11,155 @@ const TransitionComponent = ({ children }: { children: React.ReactNode }) => {
 
   const nodeRef = useRef<HTMLDivElement | null>(null);
 
-  return (
-    <SwitchTransition mode="out-in">
-      <Transition
-        nodeRef={nodeRef}
-        key={location.key ?? location.pathname}
-        timeout={500}
-        mountOnEnter
-        unmountOnExit
-        onEnter={() => {
-          toggleCompleted(false);
-          const node = nodeRef.current;
-          if (node) {
-            gsap.fromTo(
-              node,
-              {
-                autoAlpha: 0,
-                skewX: 5,
-                "--blur": 1,
-                "--brightness": 1.5,
-              },
-              {
+  if (window.innerWidth > 768) {
+    return (
+      <SwitchTransition mode="out-in">
+        <Transition
+          nodeRef={nodeRef}
+          key={location.key ?? location.pathname}
+          timeout={500}
+          mountOnEnter
+          unmountOnExit
+          onEnter={() => {
+            toggleCompleted(false);
+            const node = nodeRef.current;
+            if (node) {
+              gsap.fromTo(
+                node,
+                {
+                  autoAlpha: 0,
+                  skewX: 5,
+                  "--blur": 2,
+                  "--brightness": 1.5,
+                },
+                {
+                  autoAlpha: 1,
+                  "--blur": 0,
+                  "--brightness": 1,
+
+                  skewX: 0,
+                  duration: 1,
+                  ease: "power2.out",
+                  onUpdate: function () {
+                    if (node) {
+                      const blur = gsap.getProperty(node, "--blur");
+                      const brightness = gsap.getProperty(node, "--brightness");
+                      node.style.filter = `blur(${blur}px) brightness(${brightness})`;
+                    }
+                  },
+                  onComplete: () => toggleCompleted(true),
+                }
+              );
+            }
+          }}
+          onExit={() => {
+            const node = nodeRef.current;
+            if (node) {
+              gsap.set(node, {
                 autoAlpha: 1,
                 "--blur": 0,
                 "--brightness": 1,
-                skewX: 0,
-                duration: 1,
-                onUpdate: function () {
-                  if (node) {
-                    const blur = gsap.getProperty(node, "--blur");
-                    const brightness = gsap.getProperty(node, "--brightness");
-                    node.style.filter = `blur(${blur}px) brightness(${brightness})`;
-                  }
+              });
+              gsap
+                .timeline()
+                .to(node, {
+                  autoAlpha: 1,
+                  skewX: -5,
+                  "--blur": 2,
+                  "--brightness": 1.5,
+                  duration: 1,
+                  ease: "power2.out",
+                  onUpdate: function () {
+                    if (node) {
+                      const blur = gsap.getProperty(node, "--blur");
+                      const brightness = gsap.getProperty(node, "--brightness");
+                      node.style.filter = `blur(${blur}px) brightness(${brightness})`;
+                    }
+                  },
+                })
+                .to(node, { autoAlpha: 0, duration: 1 });
+            }
+          }}
+        >
+          <div ref={nodeRef} className="animationWrapper">
+            {children}
+          </div>
+        </Transition>
+      </SwitchTransition>
+    );
+  } else {
+    return (
+      <SwitchTransition mode="out-in">
+        <Transition
+          nodeRef={nodeRef}
+          key={location.key ?? location.pathname}
+          timeout={500}
+          mountOnEnter
+          unmountOnExit
+          onEnter={() => {
+            toggleCompleted(false);
+            const node = nodeRef.current;
+            if (node) {
+              gsap.fromTo(
+                node,
+                {
+                  autoAlpha: 0,
+                  "--blur": 2,
+                  "--brightness": 1.5,
                 },
-                onComplete: () => toggleCompleted(true),
-              }
-            );
-          }
-        }}
-        onExit={() => {
-          const node = nodeRef.current;
-          if (node) {
-            gsap
-              .timeline()
-              .to(node, {
+                {
+                  autoAlpha: 1,
+                  "--blur": 0,
+                  "--brightness": 1,
+                  duration: 1,
+                  ease: "power2.out",
+                  onUpdate: function () {
+                    if (node) {
+                      const blur = gsap.getProperty(node, "--blur");
+                      const brightness = gsap.getProperty(node, "--brightness");
+                      node.style.filter = `blur(${blur}px) brightness(${brightness})`;
+                    }
+                  },
+                  onComplete: () => toggleCompleted(true),
+                }
+              );
+            }
+          }}
+          onExit={() => {
+            const node = nodeRef.current;
+            if (node) {
+              gsap.set(node, {
                 autoAlpha: 1,
-                skewX: -5,
-                "--blur": 1,
-                "--brightness": 1.5,
-                duration: 0.5,
-                ease: "power2.out",
-                onUpdate: function () {
-                  if (node) {
-                    const blur = gsap.getProperty(node, "--blur");
-                    const brightness = gsap.getProperty(node, "--brightness");
-                    node.style.filter = `blur(${blur}px) brightness(${brightness})`;
-                  }
-                },
-              })
-              .to(node, { autoAlpha: 0, duration: 0.5 });
-          }
-        }}
-      >
-        <div ref={nodeRef} className="animationWrapper">
-          {children}
-        </div>
-      </Transition>
-    </SwitchTransition>
-  );
+                "--blur": 0,
+                "--brightness": 1,
+              });
+              gsap
+                .timeline()
+                .to(node, {
+                  autoAlpha: 1,
+                  "--blur": 2,
+                  "--brightness": 1.5,
+                  duration: 1,
+                  ease: "power2.out",
+                  onUpdate: function () {
+                    if (node) {
+                      const blur = gsap.getProperty(node, "--blur");
+                      const brightness = gsap.getProperty(node, "--brightness");
+                      node.style.filter = `blur(${blur}px) brightness(${brightness})`;
+                    }
+                  },
+                })
+                .to(node, { autoAlpha: 0, duration: 1 });
+            }
+          }}
+        >
+          <div ref={nodeRef} className="animationWrapper">
+            {children}
+          </div>
+        </Transition>
+      </SwitchTransition>
+    );
+  }
 };
 
 export default TransitionComponent;
